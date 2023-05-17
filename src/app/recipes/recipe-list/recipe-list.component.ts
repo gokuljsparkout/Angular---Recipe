ec3b21073519225b45ee0d4e87352bc13c77e326
gokuljsparkout <gokul@sparkouttech.com>
2023-05-17 14:35:52 +0530

    Passing Data with Event and Property Binsing

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -19,7 +19,15 @@ export class RecipeListComponent implements OnInit {
       'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'
     ),
   ];
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  onRecipeSelected(recipe: Recipe) {
+    console.log('RecipeSelected'+ recipe.name);
+    this.recipeWasSelected.emit(recipe);
+  }
   constructor() {}
 
   ngOnInit() {}
+
 }
