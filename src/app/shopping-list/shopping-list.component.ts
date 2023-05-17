98e49d379b5815aeb6f8ed644e50bbee2e712ff4
gokuljsparkout <gokul@sparkouttech.com>
2023-05-17 11:52:08 +0530

    Adding Navigation with Event Binding and ngIf

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredients } from '../shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent {
-
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredients[] = [
+    new Ingredients('Apple', 5),
+    new Ingredients('Orange', 5),
+    new Ingredients('Banana', 5),
+  ];
+  constructor() {}
+  ngOnInit() {}
 }
