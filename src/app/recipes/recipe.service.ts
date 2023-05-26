f669cd63e7380a9a803fa52ff379f41788b99eea
gokuljsparkout <gokul@sparkouttech.com>
2023-05-26 14:55:54 +0530

    Ingredients Added to the Recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,18 +1,21 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredients } from '../shared/ingredients.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
+      'Tasty Schnitzel',
       'This is a Test',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505',
+      [new Ingredients('Meat', 1), new Ingredients('French Fries', 20)]
     ),
     new Recipe(
       'A Test Recipe',
       'This is a Test',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505',
+      [new Ingredients('Meat', 1), new Ingredients('French Fries', 20)]
     ),
   ];
 
